9c3c500e8933b8cd35c76868d6de24ab6b773204
Owen Roth <rothowen27@gmail.com>
2021-01-04 16:52:56 -0600

    added upwork link and redirect

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,62 +1,118 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import colorlogo from '../images/Color-logo---no-background.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import colorlogo from "../images/Color-logo---no-background.svg";
 
 const Footer = () => {
-    return (
-        <footer id="footer" className="footer">
-            <div className="w-container">
-                <div className="footer-flex-container">
-                    <Link to="/" className="footer-logo-link w-inline-block"><img src={colorlogo} alt="" className="footer-image" /></Link>
-                    <div>
-                        <h2 className="footer-heading">Social Media</h2>
-                        <ul className="w-list-unstyled">
-                            <li>
-                                <a href="https://www.linkedin.com/in/owen-roth-86ba1ba4/" className="footer-link" target="_blank" rel="noopener noreferrer">Linkedin</a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/oroth8" className="footer-link" target="_blank" rel="noopener noreferrer">Github</a>
-                            </li>
-                            <li>
-                                <a href="https://www.instagram.com/rothnaldo/" className="footer-link" target="_blank" rel="noopener noreferrer">Instagram</a>
-                            </li>
-                            <li>
-                                <a href="https://trailblazer.me/id/owenroth" className="footer-link" target="_blank" rel="noopener noreferrer">Salesforce</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2 className="footer-heading">Contract Work</h2>
-                        <ul className="w-list-unstyled">
-                            <li>
-                                <a href="!#" className="footer-link" target="_blank" rel="noopener noreferrer">Upwork</a>
-                            </li>
-                            <li>
-                                <a href="!#" className="footer-link" target="_blank" rel="noopener noreferrer">Fiver</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2 className="footer-heading">Site Map</h2>
-                        <ul className="w-list-unstyled">
-                            <li>
-                                <Link to="/" className="footer-link">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about" className="footer-link">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/portfolio" className="footer-link">Portfolio</Link>
-                                <Link to="/services" className="footer-link">Services</Link>
-                                <Link to="/contact" className="footer-link">Contact</Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="text-block">Copyright © 2020 Owen-Roth. All rights reserved.</div>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer id="footer" className="footer">
+      <div className="w-container">
+        <div className="footer-flex-container">
+          <Link to="/" className="footer-logo-link w-inline-block">
+            <img src={colorlogo} alt="" className="footer-image" />
+          </Link>
+          <div>
+            <h2 className="footer-heading">Social Media</h2>
+            <ul className="w-list-unstyled">
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/owen-roth-86ba1ba4/"
+                  className="footer-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Linkedin
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/oroth8"
+                  className="footer-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.instagram.com/rothnaldo/"
+                  className="footer-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://trailblazer.me/id/owenroth"
+                  className="footer-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Salesforce
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="footer-heading">Contract Work</h2>
+            <ul className="w-list-unstyled">
+              <li>
+                <a
+                  href="https://www.upwork.com/freelancers/~018a9e37a1053aade0"
+                  className="footer-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Upwork
+                </a>
+              </li>
+              <li>
+                <a
+                  href="!#"
+                  className="footer-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Fiver
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h2 className="footer-heading">Site Map</h2>
+            <ul className="w-list-unstyled">
+              <li>
+                <Link to="/" className="footer-link">
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" className="footer-link">
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link to="/portfolio" className="footer-link">
+                  Portfolio
+                </Link>
+                <Link to="/services" className="footer-link">
+                  Services
+                </Link>
+                <Link to="/contact" className="footer-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="text-block">
+          Copyright © 2020 Owen-Roth. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
